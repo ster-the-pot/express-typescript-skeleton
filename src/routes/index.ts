0ac2e919feccefb25587d314519e537e822e32f7
Steelo <steeeeeeelo@gmail.com>
2020-05-08 02:16:54 -0500

    DOTENV: added dotenv EXPRESS: added routes folder

---
+++ src/routes/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+var indexRouter = express.Router();
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const path = require("path");
+
+//main routing file, assign's all routes
+//body parser setup
+//cors setup
+let corsOptions = {
+    origin:"http://127.0.0.1:8080",
+    credentials:true,
+}
+
+indexRouter.use(cors(corsOptions));
+indexRouter.use(bodyParser.urlencoded({ extended: false }));
+indexRouter.use(bodyParser.json());
+
+//////////INCLUDE ALL ROUTE FILES BELOW\\\\\\\\\\\
+
+export default indexRouter;
